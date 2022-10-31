cdc7fc35a43e70a918e623b881a2af968fc30c95
vladsupr <vladsupr@hotmail.com>
2022-11-01 00:39:12 +0200

    test

---
+++ WolfRendered/WolfRenderer/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+namespace WolfRenderer;
+
+public partial class MainForm : Form
+{
+	public MainForm()
+	{
+		InitializeComponent();
+	}
+
+	private void redraw()
+	{
+		var stopwatch = new Stopwatch();
+
+		var bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+		stopwatch.Start();
+
+		Renderer.PlayerAngle = Math.PI * trackBar.Value / 180 + 0.00001;
+		Renderer.Render(bitmap);
+
+		stopwatch.Stop();
+		labelMs.Text = $"{stopwatch.Elapsed.TotalMilliseconds:0.00}ms {trackBar.Value}";
+
+		var oldBitMap = pictureBox.Image;
+		pictureBox.Image = bitmap;
+		oldBitMap?.Dispose();
+	}
+
+	private void buttonStart_Click(object sender, EventArgs e)
+	{
+		redraw();
+	}
+
+	private void MainForm_ResizeEnd(object sender, EventArgs e)
+	{
+	}
+
+	private void MainForm_ResizeBegin(object sender, EventArgs e)
+	{
+	}
+
+	private void MainForm_SizeChanged(object sender, EventArgs e)
+	{
+		redraw();
+	}
+
+	private void MainForm_KeyPress(object sender, KeyPressEventArgs e)
+	{
+	}
+
+	private void trackBar_ValueChanged(object sender, EventArgs e)
+	{
+		redraw();
+	}
+}
---
+++ WolfRendered/WolfRenderer/Renderer.cs
@@ -0,0 +1,53 @@
+﻿namespace WolfRenderer;
+
+internal static class Renderer
+{
+	private static readonly byte[,] map = new byte[,]
+	{
+		{ 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0},
+		{ 0, 1, 1, 1, 0, 0},
+		{ 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0},
+		{ 0, 0, 0, 0, 0, 0},
+	};
+
+	public const double GRID_SIZE = 100;
+	public static readonly double playerX = 250;
+	public static readonly double playerY = 250;
+	public static double PlayerAngle = 30 * Math.PI / 180;
+
+	private static double RenderRay(double x, double y, double angle)
+	{
+		double distance = (300 - playerY) / Math.Sin(angle);
+
+		return distance;
+	}
+
+	public static void Render(Bitmap bitmap)
+	{
+		var random = new Random(Environment.TickCount);
+		var g = Graphics.FromImage(bitmap);
+		g.FillRectangle(Brushes.Black, Rectangle.FromLTRB(0, 0, bitmap.Width, bitmap.Height));
+
+		var deltaAngle = Math.PI / 2 / bitmap.Width;
+		var currentAngle = PlayerAngle - (bitmap.Width * deltaAngle / 2);
+		for (int dx = 0; dx < bitmap.Width; dx++)
+		{
+			var distance = RenderRay(playerX, playerY, currentAngle);
+			var height = (double)bitmap.Height * 200 / distance / Math.Tan(Math.PI / 4);
+			if (height > 0)
+			{
+				var top = (bitmap.Height - height) / 2;
+				if (top < 0) top = 0;
+
+				var bottom = (bitmap.Height + height) / 2;
+				if (bottom > bitmap.Height) bottom = bitmap.Height - 1;
+
+				g.DrawLine(Pens.White, new Point(dx, (int)top), new Point(dx, (int)bottom));
+			}
+			currentAngle += deltaAngle;
+
+		}
+	}
+}
